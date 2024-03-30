2772ddb84761c660f2ffa3940648ce40b78f6889
cristyro <95331909+cristyro@users.noreply.github.com>
2024-03-29 22:31:15 -0700

    Update sor-server.py
    Added sliding window and cleared bugs related to implementation

--- sor-server.py
+++ sor-server.py
@@ -44,6 +44,7 @@ class packet:
 
     def update_seq(self, new_seq):
         self.seq_num= new_seq
+
     def update_ack(self, new_ack):
         self.ack_num= new_ack
 
@@ -58,10 +59,13 @@ class packet:
 
 #   ---Class to handle the RDP state machine ---
 class rdp:
-    def __init__(self, connect_id):
+    def __init__(self, connect_id, addr):
+        print("init class constructor called ************\n **********\n")
         self.state= "idle"
+        self.addr= addr
         self.connection_id= connect_id
-        self.win_available= window_size
+        self.win_available= int (window_size)
+        self.window =[]
         self.current_seq= 0
         self.current_ack = 0
         self.expected_seq= 0
@@ -76,14 +80,8 @@ class rdp:
         return self.state
     
     def put_dat(self, dat):
-        global snd_buff
-        snd_buff_data = snd_buff.get(self.connection_id)
-        if snd_buff_data is None:
-            snd_buff[self.connection_id]= [dat]
-        else:
-            #print("ADDED data to snd buff", dat)
-            snd_buff_data.append(dat)
-            snd_buff[self.connection_id]= snd_buff_data
+        # Add to send buffer if there's window space; else, add to window queue
+        self.window.append(dat)
 
     def receive_connection(self):
         global snd_buff
@@ -141,9 +139,9 @@ class rdp:
         return answ
     
     # Helper to split and packet all the data according to specified payload length
-    def pack_data(self, data):
+    def pack_data(self, data, is_first_packet=True):
         global snd_buff
-        dat_string= ""
+        dat_string = ""
         remaining_space = payload_len - len(dat_string)
         chars_to_add = min(remaining_space, len(data))  # Determine how many characters to add
         dat_string += data[:chars_to_add]  # Add characters to the buffer
@@ -151,23 +149,31 @@ class rdp:
 
         if len(dat_string) == payload_len:
             # If the buffer reaches the payload length, create DAT packet and put it
-            dat_packet = packet("DAT", 0, 0, dat_string)
-            self.put_dat(dat_packet)
-            dat_string= "" #reset after putting it in the buffer
+            dat_packet = packet("DAT", self.current_seq, 0, dat_string)
+            if is_first_packet: #if it is the first packet send automatically
+                snd_buff[self.connection_id].append(dat_packet)
+            else:
+                self.put_dat(dat_packet)
 
-        else: #We were able to fit all the payload in a "small packet"
-            dat_packet= packet("DAT", 0, 0, dat_string)
-            self.put_dat(dat_packet)
-            dat_string= "" 
-   
+            dat_string = ""  # reset after putting it in the buffer
+            
+
+        else:  # We were able to fit all the payload in a "small packet"
+            dat_packet = packet("DAT", self.current_seq, 0, dat_string)
+            if is_first_packet: #if it is the first packet send automatically
+                snd_buff[self.connection_id].append(dat_packet)
+            else:
+                self.put_dat(dat_packet)
+            dat_string = "" 
 
         if remaining_data:
-            self.pack_data(remaining_data) #pack recursively 
+            self.pack_data(remaining_data, is_first_packet=False)  # pack recursively
+
         
 
     #Function were we extract each GET request provided and service it by calling helpers
     def gather_req(self, command):
-        print("NEED TO SERVICE", command)
+        #print("packing up....", command)
         patt= r'GET(?:.|\n)*?(?=GET|$)'
         matches= re.findall(patt, command) 
         for match in matches:
@@ -176,33 +182,73 @@ class rdp:
             answ= self.service_request(match)
             self.pack_data(answ)
 
+    #Determines if we need to send a RST and terminate connection
+    def to_reset(self, seq, command) -> bool:
+        seq_num = int(seq)
+        if "ACK" in command:
+            return False
+        if self.expected_seq - window_size <= seq_num <= self.expected_seq + window_size:
+            print("in expected range")
+            #If the sequence number falls within the window range, I dont need to send rest
+            return False #Do not reset
+        else:
+            print("sending reset")
+            #If its outside our range send RST
+            return True 
+    
+    def send_rst(self):
+        global snd_buff
+        global clients
+        global rcv_buff
+        print("terminating due to RST")
+        rst_pack= packet("RST", self.current_seq, self.current_ack, " ")
+        sock.sendto(str(rst_pack).encode(), self.addr)
+
+        self.terminate_conn()
+
     #determines if the sequence number is right! if it is, then receive package
-    #Otherwise, drop it
+    #Otherwise, drop it or send rst 
     def check_in_accordance(self, command) -> bool:
         command = command.decode()
         seq, ack, client_paylen = self.find_numbers(command)
-        print("SEQ, ACK, LEN", seq, ack, client_paylen) 
-        # Update expected sequence number
-        if self.expected_seq <= int(seq):
-            self.expected_seq = int(seq) + int(client_paylen)
-            print("Expected sequence number updated to:", self.expected_seq)
-            # Update current sequence number 
-            self.current_seq = int(seq)
-            print("Packet received in accordance with protocol.")
-            return True
-        else:
-            print("Packet not in accordance with protocol. Expected sequence:", self.expected_seq)
+
+        if self.to_reset(seq, command):
+            self.send_rst()
             return False
+        
+        #how do i pop stuff from the win ?
+        #release the sliding win 
+        elif "ACK" in command :
+            if int (ack) > self.current_ack:
+                self.current_ack = int(ack)
+            print("New ack is ", self.current_ack)
+            return True
+
+        else:
+            #print("received", int(seq) + int(client_paylen))
+            if self.expected_seq == int(seq):
+                    self.expected_seq = int(seq) + int(client_paylen)
+                    print("Expected sequence number updated to:", self.expected_seq)
+                    # Update current sequence number 
+                    self.current_seq = int(seq)
+                    print("Packet received in accordance with protocol.")
+                    return True
+            else:
+                print("Packet not in accordance with protocol. Expected sequence:", self.expected_seq)
+                return False
         
 
     def terminate_conn(self):
         global rcv_buff
         global snd_buff
         global clients
-        rcv_buff[self.connection_id]= []
-        snd_buff[self.connection_id]=[]
-        clients[self.connection_id]= []
 
+        if self.connection_id in snd_buff:
+            snd_buff.pop(self.connection_id)
+        if self.connection_id in clients:
+            clients.pop(self.connection_id)
+        if self.connection_id in rcv_buff:
+            rcv_buff.pop(self.connection_id)
 
 
     def to_string(self, nested_commads):
@@ -223,27 +269,61 @@ class rdp:
     #updates current_ack and current_seq accordingly
     def generate_nums(self, my_paylen, client_paylen):
         if self.first_packet:
-            self.current_seq+=my_paylen
+            self.current_seq+= my_paylen
             self.first_packet= False
         else:
             self.current_seq+= my_paylen #my paylength 
 
-        #print("CLIENTS PAYLEN", client_paylen, "MINE", self.current_ack)
+        print("MY  paylen in generate nums is:", self.current_seq)
         self.current_ack+= int(client_paylen) #acknowledge client's received packet
-        #print("AFTER UPDATE", self.current_ack)
 
+        #Send True if I reach non-zero
+    def can_send_pack(self, packet_len): #peek next packet len
+        measure_win = self.win_available - packet_len
+        return measure_win >=0
+
+
+    def release_packets(self):
+        #where window is a list of packets sorted with their seq no
+        print("in release ......\n")
+        print("In our window we have: ", len (self.window) )
+
+        while self.window!=[] and self.can_send_pack((self.window[0]).length): 
+                next_packet= self.window.pop(0) #Gets the next inmediate packet to send
+                print("next packet len?", next_packet.length)
+                print("current seq:", self.current_seq)
+                updated_packet = packet(next_packet.command, self.current_seq, self.current_ack, next_packet.payload)
+
+                if self.window:
+                    peek= self.window[0]
+                    print("peek next len?", peek.length)
+                    self.current_seq += peek.length  #peek next
+
+                print("seq no is now:", self.current_seq)
+                #how can i peek into next item in window?
+                snd_buff[self.connection_id].append(updated_packet)
 
     #Only here if we have an ACK
     #might have to add more states and transitions     
     #Returns false if ACK is in command and we want to stop parsing and skip to sending data to the client
     def process_ack(self, command)-> bool :
         global send_data
+        print("Received", command)
         if self.get_state()== "fin-sent" and "FIN in command": 
             print("CUT OFF CLIENT")
             self.set_state("conn-fin-rcv")
-            return True
-        else: #If it is just an ack for now?
-            send_data= True
+            self.terminate_conn()
+            return False
+        else: #Send as many packets as we can
+            print("release all possible packets? ")
+            print("\n our current ack_no", self.current_ack)
+            print("Releasing packets acked", len(self.window ))
+            self.release_packets()
+            print ("\n----------------------------\n")
+            self.window= [packet for packet in self.window if packet.seq_num >= self.current_ack] #Remove all packets in the window after the ack
+            print("now left with:", self.window)
+            self.expected_seq = max (self.expected_seq, self.current_ack) #update? see if causes no problems
+            #print("expected is now, updated in process_ack ", self.expected_seq)
             self.set_state("connect")
             return False
         
@@ -283,58 +363,54 @@ class rdp:
         commands= []
         string_commands= ""
         first_dat = True
-
+        
         queue_msg= snd_buff[self.connection_id]
         snd_buff[self.connection_id]=[]
 
         while queue_msg:
             p= queue_msg.pop(0)
-            client_seq, client_ack, client_paylen=  self.find_numbers(rcv_buff[self.connection_id].decode())
+            bytes_form= rcv_buff[self.connection_id]
+            string_received= bytes_form[0].decode()
+            client_seq, client_ack, client_paylen=  self.find_numbers(string_received)
 
             if p.command!= "DAT":
                 commands.append(p.command)
 
             if p.command =="DAT" and not first_dat:
+                print("HERE $1")
                 commands= ["DAT"]
 
             if p.command == "DAT" and first_dat:
+                print("HERE $2")
                 first_dat = False
                 commands.append("DAT")
+                print(self.get_state())
             
-            elif self.get_state()== "fin-rcv" and not queue_msg:
+            elif self.get_state()== "fin-rcv" : #maybe add other conditions?
+                print("HERE $3")
+                print("STATE", self.get_state())
+                print("window?", self.window)
+
                 commands.append("FIN")
 
             if (self.get_state()== "fin-rcv") and "DAT" in commands:
+                print("HERE $4")
+
                 self.generate_nums(p.length, client_paylen)
                 new_pack= packet(self.to_string(commands), self.current_seq, self.current_ack, p.payload)
                 self.set_state("fin-sent")
-                self.put_dat(new_pack)
+                snd_buff[self.connection_id].append(new_pack)
 
             if "DAT" in commands:
-                if not queue_msg: # if we reach the end
+                if not queue_msg and not self.window: # if we reach the end
+                    print("HERE ........................")
+                    print("Appending it here at the end-----")
                     commands.append("FIN")
+                    self.set_state("fin-sent")
                 self.generate_nums(p.length, client_paylen)
                 new_pack= packet(self.to_string(commands), self.current_seq, self.current_ack, p.payload)
-                self.put_dat(new_pack)
+                snd_buff[self.connection_id].append(new_pack)
 
-
-        #------------------- Helpers for writable and sending data to client ---------------------
-    """ 
-    def send_data_to_client(self, addr):
-        global snd_buff
-        global retransmit_buff
-        global send_more_data
-        info_to_send= snd_buff[self.connection_id]
-        retransmit_buff[self.connection_id]= {} #how to do it nested?
-        while info_to_send : #remember to clear snd buff after all is done
-            msg= info_to_send.pop(0)
-            seq_no = msg.seq_num
-            retransmit_buff[self.connection_id][seq_no]= msg
-            sock.sendto(str(msg).encode(), addr)
-            if ("SYN" in msg.command or "ACK" in msg.command):
-                break; """
-                #send_more_data = True if len(info_to_send)>1 else False 
-            
                   
 def udp_sock_start(ip, port):
     global sock
@@ -357,35 +433,34 @@ def main_loop():
         readable, writable, exceptional = select.select([sock], [sock], [sock], 100)
         for s in readable:
             data, addr = s.recvfrom(5000)
-            port_no= addr[1] # Extract port number from address
-            print( "Received ", data.decode())
+            port_no= addr[1] 
+            #print("Received ", data.decode())
 
             # If the client is new, initialize RDP object
             if not port_no in clients:
-                rdp_obj= rdp(port_no)
+                rdp_obj= rdp(port_no, addr)
                 clients[port_no]= rdp_obj
-                rcv_buff[port_no]= data
+                rcv_buff[port_no]= [data]
+            
 
              # Check if received packet has correct sequence number as expected by the protocol
             if rdp_obj.check_in_accordance(data):
                 continue_parse= rdp_obj.parse_command(data) #Parse command from the packet
-                print("continue parse? or skip to sending", continue_parse)
                 if continue_parse:
-                    print("IN continue to parse")
                     rdp_obj.generate_response() #Generate an appropiate response
-                    print("GENERATED response", snd_buff[rdp_obj.connection_id])
        
             # Send data to clients
-            info_to_send= snd_buff[rdp_obj.connection_id]
-            retransmit_buff[rdp_obj.connection_id]= {} #how to do it nested?
-            while info_to_send : #remember to clear snd buff after all is done
-                msg= info_to_send.pop(0)
-                print("SENDING....", msg)
-                seq_no = msg.seq_num
-                retransmit_buff[rdp_obj.connection_id][seq_no]= msg
-                sock.sendto(str(msg).encode(), addr)
-                if ("SYN" in msg.command or "ACK" in msg.command):
-                    break; 
+            if rdp_obj.connection_id in snd_buff:
+                info_to_send= snd_buff[rdp_obj.connection_id]
+                retransmit_buff[rdp_obj.connection_id]= {} 
+                while info_to_send : #remember to clear snd buff after all is done
+                    msg= info_to_send.pop(0)
+                    #print("SENDING....", msg)
+                    seq_no = msg.seq_num
+                    retransmit_buff[rdp_obj.connection_id][seq_no]= msg
+                    sock.sendto(str(msg).encode(), addr)
+                    if ("SYN" in msg.command or "ACK" in msg.command):
+                        break; 
 
 
 if __name__== "__main__":
